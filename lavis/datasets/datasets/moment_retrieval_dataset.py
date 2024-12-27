d1422c9bb419cc3edebfd425aa6ee964b6eeee84
Joel Tschesche <joel.tschesche@accso.de>
2024-12-27 02:10:41 +0100

    include charades splits, remove sr from the getitem method in moment_retrieval_dataset.py

--- lavis/datasets/datasets/moment_retrieval_dataset.py
+++ lavis/datasets/datasets/moment_retrieval_dataset.py
@@ -28,7 +28,7 @@ class MomentRetrievalDataset(BaseDataset):
         vname = ann["video"]
         video_path = os.path.join(self.vis_root, vname + ".mp4")
 
-        frms, indices, fps, audio, sr = self.vis_processor(video_path, clip_proposal=clip)
+        frms, indices, fps, audio = self.vis_processor(video_path, clip_proposal=clip)
         query = ann["query"]
         relevant_windows = str(ann["relevant_windows"])
 
