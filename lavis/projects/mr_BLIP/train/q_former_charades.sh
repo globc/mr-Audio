ee261636d9fab5d627104d422bda5c54b0d1bec5
Joel Tschesche <joel.tschesche@accso.de>
2025-02-11 10:32:52 +0100

    add new run script for charades with q-former and update paths

---
+++ lavis/projects/mr_BLIP/train/q_former_charades.sh
@@ -0,0 +1,7 @@
+export CUDA_VISIBLE_DEVICES=$(nvidia-smi --query-gpu=index --format=csv,noheader | tr '\n' ',' | sed 's/,$//')
+echo $CUDA_VISIBLE_DEVICES
+
+export NUM_GPUS=${SLURM_GPUS_ON_NODE:-$(nvidia-smi -L | wc -l)}
+echo $NUM_GPUS
+
+CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES CUDA_LAUNCH_BLOCKING=1 python -m torch.distributed.run --nproc_per_node=$NUM_GPUS train.py --cfg-path lavis/projects/mr_Audio/train/blip2_mr_audio_xinstructblip/charades_sta.yaml
