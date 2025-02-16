d3b7117a7ed47988d85c7d4dbcc20a54be555e93
Ahmed <minatocss@gmail.com>
2025-02-16 18:49:41 +0100

    fix job

--- checkpoint_job.sh
+++ checkpoint_job.sh
@@ -17,15 +17,11 @@ export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 #ml gcc/11 python/3.8 cuda/11.8
 #ml gcc/11 python/3.9-anaconda cuda/12.1
 
-#source $WORK/venvs/mrBlipAudio/bin/activate
-conda init
-conda activate mrAudioConda
-
 export http_proxy=http://proxy:80
 export https_proxy=http://proxy:80
 
 ml gcc/11 cuda/12.1.1 cudnn/8.9.6.50-12.x
 #conda activate mrBlipAudio
 
-./run_scripts/mr_BLIP/train/charades_sta.sh
+./run_scripts/mr_BLIP/train/charades.sh
 #conda deactivate
