df597df8ac92c4eb2d05ecddd4b947e262e2cf2a
Ahmed <minatocss@gmail.com>
2025-02-17 19:53:35 +0100

    fix sh

--- checkpoint_job.sh
+++ checkpoint_job.sh
@@ -2,30 +2,29 @@
 #SBATCH --job-name=mr_audio_debug
 #SBATCH --time=4:00:00
 #SBATCH --gres=gpu:a100:2 -C a100_80
-
 #SBATCH --output=logs/job%j%x.out
 #SBATCH --error=logs/job%j_%x.err
 #SBATCH --mail-user=minatocss@gmail.com
 #SBATCH --mail-type=END,FAIL                     # Options: BEGIN, END, FAIL, or ALL
 
-
+# Unset SLURM_EXPORT_ENV to prevent automatic export of environment variables
 unset SLURM_EXPORT_ENV
 
+# Set OMP_NUM_THREADS based on SLURM_CPUS_PER_TASK
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
-#export CUDA_NUM_DEVICES=$SLURM_GPUS_ON_NODE
 
-#ml gcc/11 python/3.8 cuda/11.8
-#ml gcc/11 python/3.9-anaconda cuda/12.1
+# Load required modules for the job (uncomment and modify if necessary)
+module load gcc/11 cuda/12.1.1 cudnn/8.9.6.50-12.x
 
-#source $WORK/venvs/mrBlipAudio/bin/activate
-conda init
-conda activate mrAudioConda
+# Activate Conda environment for your job
+source ~/miniconda3/bin/activate mrAudioConda
 
+# Set proxy environment variables if required (remove if not needed)
 export http_proxy=http://proxy:80
 export https_proxy=http://proxy:80
 
-ml gcc/11 cuda/12.1.1 cudnn/8.9.6.50-12.x
-#conda activate mrBlipAudio
-
+# Run the training script
 ./run_scripts/mr_BLIP/train/charades.sh
-#conda deactivate
+
+# Optionally deactivate conda at the end (if desired)
+# conda deactivate
