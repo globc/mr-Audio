ecaed98d7a04da6e2c53dddbaa86c0136fa4a578
Joel Tschesche <joel.tschesche@accso.de>
2025-02-27 17:50:00 +0100

    remove autocast to GatedCrossAttention.py

--- lavis/models/blip2_mr_models/GatedCrossAttention.py
+++ lavis/models/blip2_mr_models/GatedCrossAttention.py
@@ -19,18 +19,17 @@ class GatedCrossAttention(nn.Module):
         vision_emb: [batch_size, seq_len, vision_dim]
         audio_emb:  [batch_size, seq_len, audio_dim]
         """
-        with torch.cuda.amp.autocast():
-            # Project audio features to match vision feature size
-            # comment line out for fusion before projection
-            audio_emb = self.audio_proj(audio_emb)
+        # Project audio features to match vision feature size
+        # comment line out for fusion before projection
+        audio_emb = self.audio_proj(audio_emb)
 
-            # Compute cross-attention (audio attending to video)
-            attended_audio, _ = self.cross_attn(audio_emb, vision_emb, vision_emb)
+        # Compute cross-attention (audio attending to video)
+        attended_audio, _ = self.cross_attn(audio_emb, vision_emb, vision_emb)
 
-            # Compute gating scores (importance of each token)
-            gate_values = torch.sigmoid(self.gate(audio_emb)).float()
+        # Compute gating scores (importance of each token)
+        gate_values = torch.sigmoid(self.gate(audio_emb))
 
-            # Apply gating mechanism
-            fused_representation = gate_values * attended_audio + (1 - gate_values) * vision_emb  # Selectively fuse
+        # Apply gating mechanism
+        fused_representation = gate_values * attended_audio + (1 - gate_values) * vision_emb  # Selectively fuse
 
         return fused_representation
