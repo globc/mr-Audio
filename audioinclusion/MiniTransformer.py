75b96755817bf360b9f2e4a15a6a8375df419b9d
Habibi <h.maraqten@gmail.com>
2025-03-24 13:27:45 +0100

    added small docstring

--- audioinclusion/MiniTransformer.py
+++ audioinclusion/MiniTransformer.py
@@ -2,13 +2,21 @@ import torch
 import torch.nn as nn
 
 class ImageAudioFusion(nn.Module):
+    """Experimental Fusion Method. This class was not used to produce the results in the paper.
+    This module fuses image and audio features using an attention mechanism.
+    Fusion happens at audio embedding dimension.
+    Args:
+        embed_dim: embedding dimension
+        num_heads: number of attention
+
+    """
     def __init__(self, embed_dim=512, num_heads=8):
         super().__init__()
 
         self.attn = nn.MultiheadAttention(embed_dim, num_heads)
         self.linear = nn.Linear(embed_dim, embed_dim)
 
-        
+
     def forward(self, concat_vector):
         """
         image_feats: (batch_size,embed_dim) e.g. [32,512] (latter is audio encoder output len
